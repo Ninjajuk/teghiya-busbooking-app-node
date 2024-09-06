2174c512f86773f7b2100681eb7e3f9e1f02e77c
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-09-06 21:55:25 +0530

    database connection

--- database/db.js
+++ database/db.js
@@ -1,9 +1,20 @@
+const { MongoClient } = require("mongodb");
 
+const uri = process.env.DATABASE_URL;
 
-async function connectToDb(){
-    try {
-        const 
-    } catch (error) {
-        
-    }
+// Database name
+const dbName = "mydatabase";
+
+// Create a new MongoClient
+const client = new MongoClient(uri);
+
+async function connectToDb() {
+  try {
+    await client.connect(); // Connect the client to the server
+    console.log("Connected successfully to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  } finally {
+    await client.close(); // Close the connection
+  }
 }
