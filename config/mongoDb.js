f1df9beddb8ab51d6112b6e971ba5a0ae90ad2aa
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-09-11 00:32:28 +0530

    redis and mongodb connected

--- config/mongoDb.js
+++ config/mongoDb.js
@@ -1,17 +1,28 @@
 // const { MongoClient } = require("mongodb");
 const mongoose = require("mongoose");
+const redis = require("redis");
 require("dotenv").config();
 
 const uri = process.env.DATABASE_URL;
 
 // Create a new MongoClient
 // const client = new MongoClient(uri);
+const redisClient = redis.createClient({
+  url: process.env.REDIS_URL
+});
 
 async function connectToDb() {
   try {
     await mongoose.connect(process.env.DATABASE_URL); //Connection using mongoose
     // await client.connect(); // Connect the client to the server
     console.log("Connected successfully to MongoDB");
+
+        // Connect to Redis
+        await redisClient.connect();
+        console.log(`Connected successfully to Redis at port ${process.env.REDIS_URL}`);
+    
+        // Handling Redis errors
+        redisClient.on("error", (err) => console.log("Redis Client Error", err));
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
