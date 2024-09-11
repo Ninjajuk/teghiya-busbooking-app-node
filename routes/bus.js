11c3dd0e811912c3de576538e585572ec8343029
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-09-11 22:01:11 +0530

    bus route addded

--- routes/bus.js
+++ routes/bus.js
@@ -6,6 +6,11 @@ const router=express.Router()
 router
      .post('/api/v1/addNewBus',addNewBus)
      .post('/api/v1/getBusById',getBusById)
+     .post('/api/v1/getAllBuses')   //Retrieve a list of all buses.
+     .post('/api/v1/updateBus')    // Update details of a specific bus.
+     .post('/api/v1/deleteBus',)   //Remove a specific bus.
 
 
 module.exports=router
+
+
