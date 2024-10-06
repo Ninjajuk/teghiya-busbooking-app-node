b778eb864c8da5a4ecc6bbd56ec282f655533106
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-10-06 20:49:47 +0530

    bustypes

--- controllers/BusType.js
+++ controllers/BusType.js
@@ -2,7 +2,8 @@ const BusType = require( '../model/busTypeSchema')
 
 exports.addBusType = async (req, res) => {
     try {
-
+        const { busModelName } = req.body
+        if (!busModelName) return res.json({ reason: 'busModelName should not be empty' })
         const newBustype = new BusType(req.body)
         // console.log('addBusType', newBustype)
         await newBustype.save()
