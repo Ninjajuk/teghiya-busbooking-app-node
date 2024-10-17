193c6106e7a7f0d5dfe6a1406c8b8de7b5979001
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2024-10-17 20:41:48 +0530

    seatlayout

--- controllers/seatLayout.js
+++ controllers/seatLayout.js
@@ -2,6 +2,8 @@
 
 exports.createBusSeatLayout = async(req, res) => {
     try {
+        const {busId} = req.body
+        if(!busId) return res.json({reason:'busId should not be empty'})
         
     } catch (error) {
         
